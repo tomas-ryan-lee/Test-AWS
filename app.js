d4a5534e2443fdd73c564c4a4d4e4c1d57d7d982
Kev <rifakev@gmail.com>
2021-02-17 16:49:20 +0100

    Codage du JWS

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ var db = require('./db');
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
 
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+
 module.exports = app;
+module.exports = router;
